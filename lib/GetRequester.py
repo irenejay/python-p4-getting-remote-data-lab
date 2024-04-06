6022ad90a7bb17af27fa92c42f7bf26f82ca6f05
irene <irenejim9504@gmail.com>
2024-04-06 22:27:38 +0300

    completed the remote data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,23 @@
 import requests
 import json
-
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
-
+        response = requests.get(self.url)
+       
+        return response.content
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            json_data = json.loads(response_body)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            return None
+        return json_data
+  
+    
+    
+    
+    
